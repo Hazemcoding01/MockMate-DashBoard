b3a417fe02a6cd5a973eb557e5090178fc7ef74e
Hazemcoding01 <ethhazem@gmail.com>
2026-04-23 16:21:23 +0200

    fix api url

--- public/login.js
+++ public/login.js
@@ -16,7 +16,7 @@ form.addEventListener('submit', async function (e) {
   }
 
   try {
-    const response = await fetch('/api/users/login', {
+    fetch('http://mockmate-001-site1.mtempurl.com/api/users/login',  {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email, password })
